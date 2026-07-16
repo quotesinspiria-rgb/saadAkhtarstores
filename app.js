024f1ddac541f8d56281314ef90c93e7ec1a4d5e
quotesinspiria-rgb <quotesinspiria@gmail.com>
2026-07-17 00:33:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -696,3 +696,62 @@ cartOverlay.onclick=closeCart;
 renderGrid();
 
 renderCart();
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides(){
+
+    let slides = document.getElementsByClassName("slides");
+
+    for(let i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+
+    slideIndex++;
+
+    if(slideIndex>slides.length){
+        slideIndex=1;
+    }
+
+    slides[slideIndex-1].style.display="block";
+
+    setTimeout(showSlides,3000);
+
+}
+
+document.querySelector(".next").onclick=function(){
+
+    slideIndex++;
+
+    showManual();
+
+};
+
+document.querySelector(".prev").onclick=function(){
+
+    slideIndex--;
+
+    showManual();
+
+};
+
+function showManual(){
+
+    let slides=document.getElementsByClassName("slides");
+
+    if(slideIndex>slides.length){
+        slideIndex=1;
+    }
+
+    if(slideIndex<1){
+        slideIndex=slides.length;
+    }
+
+    for(let i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+
+    slides[slideIndex-1].style.display="block";
+
+}
