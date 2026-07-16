71a96847d5eedc46e3f044fe46058c390e5008d1
quotesinspiria-rgb <quotesinspiria@gmail.com>
2026-07-17 00:54:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -755,3 +755,66 @@ function showManual(){
     slides[slideIndex-1].style.display="block";
 
 }
+// =========================
+// HOME SLIDER
+// =========================
+
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides(){
+
+    const slides = document.getElementsByClassName("slide");
+
+    for(let i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+
+    slideIndex++;
+
+    if(slideIndex>slides.length){
+        slideIndex=1;
+    }
+
+    slides[slideIndex-1].style.display="block";
+
+    setTimeout(showSlides,3000);
+
+}
+
+document.querySelector(".next").onclick=function(){
+
+    slideIndex++;
+
+    manualSlide();
+
+}
+
+document.querySelector(".prev").onclick=function(){
+
+    slideIndex--;
+
+    manualSlide();
+
+}
+
+function manualSlide(){
+
+    const slides=document.getElementsByClassName("slide");
+
+    if(slideIndex>slides.length){
+        slideIndex=1;
+    }
+
+    if(slideIndex<1){
+        slideIndex=slides.length;
+    }
+
+    for(let i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+
+    slides[slideIndex-1].style.display="block";
+
+}
